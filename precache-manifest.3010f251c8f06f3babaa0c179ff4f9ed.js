d94f856db758b768ecb74d2313e0d84438205462
Megan Akiba <megan.akiba@colorado.edu>
2019-12-11 20:35:56 -0700

    Updates

---
+++ precache-manifest.3010f251c8f06f3babaa0c179ff4f9ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "099d553af94a6dced11ae8f08967b4a5",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "5a7e2baaf3540cf5e5ef",
+    "url": "/calculator/static/css/main.0cc4ef5e.chunk.css"
+  },
+  {
+    "revision": "0d2263dc74e008aedd0a",
+    "url": "/calculator/static/js/2.62c0a815.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/calculator/static/js/2.62c0a815.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5a7e2baaf3540cf5e5ef",
+    "url": "/calculator/static/js/main.a4494690.chunk.js"
+  },
+  {
+    "revision": "c3d9a3bbcb6bc1177d26",
+    "url": "/calculator/static/js/runtime-main.250539d0.js"
+  }
+]);
